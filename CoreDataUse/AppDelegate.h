5a0dca6e8d3477e1d7d321ba20470cb3bac93ba3
泽泽 <zezefamily@163.com>
2015-12-03 17:41:25 +0800

    Initial Commit

---
+++ CoreDataUse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataUse
+//
+//  Created by zezefamily on 15/12/3.
+//  Copyright © 2015年 zezefamily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataUse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoreDataUse
+//
+//  Created by zezefamily on 15/12/3.
+//  Copyright © 2015年 zezefamily. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
